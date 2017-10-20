a4e2c1670346b8160c2efe7b6c3cc9ddb951d8ed
Sachin Chandy <sachinmchandy@gmail.com>
2017-10-20 12:48:51 +0100

    medium_1

---
+++ highest_and_lowest_ranking_cards.rb
@@ -0,0 +1,56 @@
+# highest and lowest ranking cards from an array of card objects
+
+RANKS = { 'Ace' => 14, 'King' => 13, 'Queen' => 12, 'Jack' => 11,
+            10 => 10, 9 => 9, 8 => 8, 7 => 7, 6 => 6, 5 => 5, 4 => 4,
+            3 => 3, 2 => 2 }
+
+class Card
+  attr_reader :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def <=>(other)
+    RANKS[rank] <=> RANKS[other.rank]
+  end
+
+  def ==(other)
+    rank == other.rank && suit == other.suit
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+end
+
+
+cards = [Card.new(2, 'Hearts'),
+         Card.new(10, 'Diamonds'),
+         Card.new('Ace', 'Clubs')]
+puts cards
+puts cards.min == Card.new(2, 'Hearts')
+puts cards.max == Card.new('Ace', 'Clubs')
+
+cards = [Card.new(5, 'Hearts')]
+puts cards.min == Card.new(5, 'Hearts')
+puts cards.max == Card.new(5, 'Hearts')
+
+cards = [Card.new(4, 'Hearts'),
+         Card.new(4, 'Diamonds'),
+         Card.new(10, 'Clubs')]
+puts cards.min.rank == 4
+puts cards.max == Card.new(10, 'Clubs')
+
+cards = [Card.new(7, 'Diamonds'),
+         Card.new('Jack', 'Diamonds'),
+         Card.new('Jack', 'Spades')]
+puts cards.min == Card.new(7, 'Diamonds')
+puts cards.max.rank == 'Jack'
+
+cards = [Card.new(8, 'Diamonds'),
+         Card.new(8, 'Clubs'),
+         Card.new(8, 'Spades')]
+puts cards.min.rank == 8
+puts cards.max.rank == 8
