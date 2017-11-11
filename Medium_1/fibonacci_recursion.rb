10746073bd0451c031efca38db8a5f24d7b8ac0b
Sachin Chandy <sachinmchandy@gmail.com>
2017-11-11 22:21:30 +0000

    fibonacci recursive

---
+++ Medium_1/fibonacci_recursion.rb
@@ -0,0 +1,12 @@
+def fibonacci(nth)
+  return 1 if nth <= 2
+  fibonacci(nth - 1) + fibonacci(nth - 2)
+end
+
+p fibonacci(1) == 1
+p fibonacci(2) == 1
+p fibonacci(3) == 2
+p fibonacci(4) == 3
+p fibonacci(5) == 5
+p fibonacci(12) == 144
+p fibonacci(20) == 6765
