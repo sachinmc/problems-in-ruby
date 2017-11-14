704f54fc97c22d0bdd2d4bab1a1cf0d82a71be03
Sachin Chandy <sachinmchandy@gmail.com>
2017-11-14 09:38:02 +0000

    multiple assignment, procedural

--- Medium_1/fibonacci_procedural.rb
+++ Medium_1/fibonacci_procedural.rb
@@ -1,18 +1,31 @@
+# def fibonacci(nth)
+#   a = 1
+#   b = 1
+#   count = 3 # start counting from 3rd fib# since 1st and 2nd fib# is 1
+#   result = 1 # 1st and 2nd fib#'s are 1
+#   return result if nth == 1 || nth == 2
+#   loop do
+#     a = b # a is previous b
+#     b = result # b is previous result
+#     result = a + b # result is sum of previous 2 numbers
+#     break if count == nth
+#     count += 1
+#   end
+#   return result
+# end
+
+
+# LS Solution
 def fibonacci(nth)
-  a = 1
-  b = 1
-  count = 3 # start counting from 3rd fib# since 1st and 2nd fib# is 1
-  result = 1 # result is 1 for 1st and 2nd fib#'s
-  return result if nth == 1 || nth == 2
-  loop do
-    a = b # a is previous b
-    b = result # b is previous result
-    result = a + b # result is sum of previous 2 numbers
-    break if count == nth
-    count += 1
+  first, last = [1, 1]
+  3.upto(nth) do
+    first, last = [last, first + last]
   end
-  return result
+  last
 end
+
+
+
 
 p fibonacci(1) == 1
 p fibonacci(2) == 1
