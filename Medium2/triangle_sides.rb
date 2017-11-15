9547c48c4b173aaa4d9f9888a267a81da5c213ff
Sachin Chandy <sachinmchandy@gmail.com>
2017-11-15 16:54:10 +0000

    case stmt, Array#max, Array#sort, *args

---
+++ Medium2/triangle_sides.rb
@@ -0,0 +1,33 @@
+def triangle(*sides)
+  sides.sort!
+  return :invalid if sides.include?(0) || sides[0] + sides[1] < sides[2]
+  case sides.uniq.length
+    when 1 then :equilateral
+    when 2 then :isosceles
+    when 3 then :scalene
+  end
+end
+
+
+p triangle(3, 3, 3) == :equilateral
+p triangle(3, 3, 1.5) == :isosceles
+p triangle(3, 4, 5) == :scalene
+p triangle(0, 3, 3) == :invalid
+p triangle(3, 1, 1) == :invalid
+
+# LS solution
+def triangle(side1, side2, side3)
+  sides = [side1, side2, side3]
+  largest_side = sides.max
+
+  case
+  when 2 * largest_side > sides.reduce(:+), sides.include?(0)
+    :invalid
+  when side1 == side2 && side2 == side3
+    :equilateral
+  when side1 == side2 || side1 == side3 || side2 == side3
+    :isosceles
+  else
+    :scalene
+  end
+end 
